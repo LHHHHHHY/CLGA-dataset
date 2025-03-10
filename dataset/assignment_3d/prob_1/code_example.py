fc2a149e011a09222f2e8b58e0404d29d18785ed
LHHHHHHY <1264901730@qq.com>
2025-03-10 19:47:02 +0800

    update

---
+++ dataset/assignment_3d/prob_1/code_example.py
@@ -0,0 +1,11 @@
+def prob_1(defender_pos, intruder_pos): 
+    """
+    Args:
+        defender_pos: A list of tuples representing the positions (x, y, z) of each defender.
+        intruder_pos: A list of tuples representing the positions (x, y, z) of each intruder.
+    Returns:
+        assignment: List of task assignments where each defender is assigned an intruder by index.
+    """
+    # To be implemented
+    assignment = [0,1,2,3,4,5,6,7,8,9]
+    return assignment
---
+++ dataset/assignment_3d/prob_16/code_example.py
@@ -0,0 +1,10 @@
+def prob_16(assignments): 
+    """
+    Args:
+        assignments: List of task assignments where each defender is assigned an intruder by index.
+    Returns:
+        assignment: List of task assignments where each defender is assigned an intruder by index.
+    """
+    # To be implemented
+    assignment = assignments
+    return assignment
